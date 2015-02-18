48810477f77a519331313114d64063246c5fc41c
Nikita <nikitatrukhanov@MacBook-Pro-Nikita.local>
2015-02-18 14:14:49 +0500

    Finish Devise authentication

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name 'valid_name'
+    email 'valid@email.com'
+    password 'validpassowrd'
+    password_confirmation 'validpassowrd'
+  end
+
+end
