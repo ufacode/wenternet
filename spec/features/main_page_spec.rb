a302272b70cf72402a1ee49e98d740ddb4668af6
Nikita <nikitatrukhanov@MacBook-Pro-Nikita.local>
2015-03-05 20:06:56 +0500

    Finish main page

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 describe "search form on main page", type: :feature do
   it "input for search and button exist" do
     visit "/"
-    expect(page).to have_content "Искать"
-    expect(page).to have_selector "input[name=q]"
+    expect(page).to have_button "Искать"
+    expect(page).to have_selector "input[name='search[query]']"
+    expect(page).to have_selector "div.block"
   end
 end
